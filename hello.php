91e6a77271f0f6562a97143339374a7b90bddada
Tankania <114451473+Tankania@users.noreply.github.com>
2022-10-06 03:16:41 -0500

    Actividad 1

---
+++ hello.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="style2.css">
+<link rel="stylesheet" href="style2.css">
+<?php
+echo "<br>";echo "<br>";echo "<br>";
+print ('HOLAAAAAA A TODOS  MUNDOO');
+ echo "<br>";
+print ( "Bienvenidos a mi página PHP!!!");
+echo "<br>";
+print ( "Soy Tania Alvarez Aguiano");
+echo "<br>";
+print ( "Y estoy en la especialidad de programación y a veces me saca canas verdes los códigos!:)");
+echo "<br>";
+
+
+?>
