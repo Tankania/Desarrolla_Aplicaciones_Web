2a3da5ff4cb9537602c9ee2f5843c76f533bb5b5
Tankania <114451473+Tankania@users.noreply.github.com>
2022-10-06 03:26:37 -0500

    Actividad 2

---
+++ hoy.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DIA</title>
+   <link rel="stylesheet" href="miestilo.css">  
+</head>
+
+<body>
+
+<?php
+echo "<br>";echo "<br>";echo "<br>";echo "<br>";
+echo date('d-m-y');
+echo "<br>";
+
+print ( "Bienvenidos a mi página PHP!!!");
+echo "<br>";
+print ( "Hola a TODOS!!!");
+echo "<br>";
+print ( "Mi nombre es Tania Alvarez Anguiano");
+echo "<br>";
+print ( "Estoy en la especialidad de programación en el grado de 5O");
+echo "<br>";
+print ( "Desarrolla aplicaciones Web con conexión a bases de datos");
+echo "<br>";
+echo date('d-m-y');
+echo "<br>";
+print ( "PHP es muy cool de usar ya que uno tiene la libertad de usarlo y lo interesante es que wikipedia utiliza PHP!");
+echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";
+  ?>  
+</body>
+</html>
